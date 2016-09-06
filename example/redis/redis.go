31335476ae174cb48e4329207a505a7b4cd65e11
shaozhenyu <18217258834@163.com>
2016-09-06 11:26:02 +0800

    modify

--- example/redis/redis.go
+++ example/redis/redis.go
@@ -1,5 +1,45 @@
 package main
 
 import (
-	"gopkg.in/redis.v2"
+	"fmt"
+
+	"libs/cache"
 )
+
+func main() {
+	RedisHost := "127.0.0.1:6379"
+	rd_cache, err := cache.New(RedisHost, 0, 100)
+	if err != nil {
+		return
+	}
+
+	if ok := Set("a", "szy", rd_cache); !ok {
+		fmt.Println("set k-v error")
+		return
+	}
+
+	var v string
+	var ok bool
+	if v, ok = Get("a", rd_cache); !ok {
+		fmt.Println("get k-v error")
+		return
+	}
+
+	fmt.Println("value: ", v)
+}
+
+func Get(key string, rds cache.CacheStorage) (string, bool) {
+	bs, err := rds.Get(key)
+	if err != nil {
+		return "", false
+	}
+	value := string(bs)
+	return value, true
+}
+
+func Set(key, value string, rds cache.CacheStorage) bool {
+	if err := rds.Set(key, []byte(value)); err != nil {
+		return false
+	}
+	return true
+}
